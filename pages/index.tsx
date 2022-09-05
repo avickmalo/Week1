5991be80a98f1e19c88fbc2328b062b3055dbefa
Avick Malo <avickmalo@gmail.com>
2022-09-05 15:33:53 -0400

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import Head from "next/head";
 export default function Index() {
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+            <head>
+                <title> Clocky</title>
+                <meta name = "description" content = "track/"></meta>
+            </head>
+           
+           
+            <h1> Clocky </h1>
+            <h3> Your Personal Timekeeper</h3>
+                <div id = "display">
+                    <h2> Daily</h2>
+                    <p>
+                        Total elapsed time:
+                        Past 12 hours:
+                    </p>
+                    <h2> Weekly</h2>
+                    <p>
+                        Total elapsed time:
+                        Past 12 hours:
+                    </p>
+                    <h2> Categories </h2>
+                    <h2> Limits </h2>
+                   
+                    
+                </div>
+
+
+
+            
         </>
     );
 }
